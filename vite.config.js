d60255c9651cfb5f4b1271ff1b437ac2551a8a38
BrettSEvans <BrettEvansSF@gmail.com>
2026-06-04 19:04:22 -0700

    Add Vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    base: process.env.GITHUB_PAGES === 'true' ? '/dexter-project/' : '/',
+    plugins: [react()],
+    test: {
+          environment: 'jsdom',
+          setupFiles: ['./src/test-setup.js'],
+          globals: true,
+    },
+});
